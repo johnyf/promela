1bd2df05dc8fd71d088a731fae356f6e54782a39
Ioannis Filippidis <jfilippidis@gmail.com>
2021-05-31 22:47:36 +0200

    DRAFT: REL: support Python 3.9, 3.10

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Scientific/Engineering']
 KEYWORDS = [
     'promela', 'parser', 'syntax tree', 'ply', 'lex', 'yacc']
